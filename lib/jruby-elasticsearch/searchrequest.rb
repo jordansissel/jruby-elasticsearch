a250e53f92cf41d93dfb0edf1b6d97d0991e95ee
Jordan Sissel <jls@semicomplete.com>
2011-03-25 22:18:48 -0700

    - add sort and query

--- lib/jruby-elasticsearch/searchrequest.rb
+++ lib/jruby-elasticsearch/searchrequest.rb
@@ -3,11 +3,12 @@ require "jruby-elasticsearch/request"
 
 class ElasticSearch::SearchRequest < ElasticSearch::Request
   # Create a new index request.
+  public
   def initialize(client)
     @client = client
     @prep = @client.prepareSearch("example")
     super()
-  end
+  end # def initialize
 
   # Execute this search request.
   # This call is asynchronous.
@@ -16,6 +17,7 @@ class ElasticSearch::SearchRequest < ElasticSearch::Request
   #
   # On success, callback will receive a
   # org.elasticsearch.action.search.SearchResponse
+  public
   def execute(&block)
     use_callback(&block) if block_given?
 
@@ -25,11 +27,28 @@ class ElasticSearch::SearchRequest < ElasticSearch::Request
 
     action = @prep.execute(@handler)
     return action
-  end
+  end # def execute
 
   # Execute this index request synchronously
   # Returns org.elasticsearch.action.search.SearchResponse
+  public
   def execute!
     return @prep.execute.actionGet()
-  end
+  end # def execute!
+
+  public
+  def sort(field, order)
+    case order
+      when :asc
+        order_val = org.elasticsearch.search.sort.SortOrder::ASC
+      when :desc
+        order_val = org.elasticsearch.search.sort.SortOrder::DESC
+    end
+    return @prep.addSort(field, order_val)
+  end # def sort
+
+  public
+  def query(query_string)
+    return @prep.setQuery(query_string)
+  end # def query
 end # class ElasticSearch::SearchRequest
