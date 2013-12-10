b7b9aa9b87105d6b3554d237fb132973774a98da
Aaron Mildenstein <aaron@mildensteins.com>
2013-12-10 10:32:20 -0600

    Added PutTemplate section here.  Still raw and rudimentary

--- lib/jruby-elasticsearch/templaterequest.rb
+++ lib/jruby-elasticsearch/templaterequest.rb
@@ -74,3 +74,43 @@ class ElasticSearch::GetIndexTemplateRequest < ElasticSearch::Request
   end # def execute
 
 end # class ElasticSearch::GetIndexTemplateRequest
+
+class ElasticSearch::PutIndexTemplateRequest < ElasticSearch::Request
+
+  # Create a new PutIndexTemplateRequest request.
+  public
+  def initialize(client, template_name, mapping_json)
+    @client = client
+    @template_name = template_name
+    @mapping_json = mapping_json
+    begin
+      @prep = org.elasticsearch.action.admin.indices.template.put.PutIndexTemplateRequestBuilder.new(@client, @template_name)
+    rescue NameError
+      @logger.error("Could not create PutIndexTemplateRequestBuilder", :name_error => NameError.to_s)
+    end
+    super()
+    # Assign the template
+    @prep.setSettings(@mapping_json)
+  end # def initialize
+
+  public
+  def with(&block)
+    instance_eval(&block)
+    return self
+  end # def with
+
+  # Execute this request.
+  # This call is asynchronous.
+  #
+  # If a block is given, register it for both failure and success.
+  #
+  # On success, callback will receive a
+  # org.elasticsearch.action.admin.indices.template.get.GetIndexTemplateResponse
+  public
+  def execute(&block)
+    use_callback(&block) if block_given?
+    action = @prep.doExecute(@handler)
+    return action
+  end # def execute
+
+end # class ElasticSearch::PutIndexTemplateRequest
